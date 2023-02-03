d1d388599d7803775b703468993a0053c54a08aa
Tony McDowell <svet.am@gmail.com>
2023-02-03 10:08:09 -0700

    main.py: create a real-world example of processing ACF files in a loop

--- main.py
+++ main.py
@@ -2,13 +2,22 @@
 from steamfile import steamfile
 import os
 
-# Create path to acf file
-directory = os.path.dirname(os.path.realpath(__file__))
-acf_dir = directory + '\\appmanifest_730.acf'
+# Process a directory of ACF files
+directory = "C://Program Files (x86)//Steam//steamapps//"
+for filename in os.listdir(directory):
+    if filename.endswith(".acf"):
+        print("Processing file: " + filename)
+        acf = steamfile()
+        acf_gamefile = acf.load(directory+filename)
+        print("Game: " + acf_gamefile["name"] + " (" + acf_gamefile["appid"] + ")")
+
+
+# directory = os.path.dirname(os.path.realpath(__file__))
+# acf_dir = directory + '\\appmanifest_730.acf'
 
 # Initialize steamfile() and use method load()
 # load() would return a dict data type of the acf file
-acf = steamfile()
-acf_gamefile = acf.load(acf_dir)
+# acf = steamfile()
+# acf_gamefile = acf.load(acf_dir)
 
-print(acf_gamefile)
+# print(acf_gamefile)
